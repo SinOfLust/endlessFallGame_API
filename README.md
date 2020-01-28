21bfdf1fbef55e63b8566bc05724bfa1e043f179
Benjamin Champetier <Benjamin.Champetier@intitek.fr>
2020-01-28 15:50:58 +0100

    ajout branche benjamin

---
+++ README.md
@@ -0,0 +1,7 @@
+# My Endless Game API with GraphQL
+
+## Installation
+```
+$ npm install
+$ npm run dev
+```
--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const { schema } = require('./src/graphql/schema')
-const { root } = require('./src/graphql/root')
-
+const { schema } = require('./src/graphql/schema');
+const { root } = require('./src/graphql/root');
+const port = process.env.PORT || 4000;
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
-}));
+// GraphQL
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		schema: schema,
+		rootValue: root,
+		graphiql: true
+	})
+);
 
-app.listen(4000);
+// Server
+app.listen(port, () => console.log(`Server listening on port: ${port}`));
