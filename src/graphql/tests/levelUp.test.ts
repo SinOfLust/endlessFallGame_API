c38443ec94d75ed7a0a583ea13eaa1f699cd2e5e
loydfassi <loyd.fassi@gmail.com>
2020-02-01 03:06:52 +0100

    comment

--- src/graphql/tests/levelUp.test.ts
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-import mutation from '../mutations'
-
-test('resolver levelUp return the old level + 1', () => {
-
-    const oldDatas: { level: number } = JSON.parse(fs.readFileSync('src/database/data.json') as unknown as string)
-
-    expect(mutation.levelUp()).toBe(oldDatas.level + 1)
-})
